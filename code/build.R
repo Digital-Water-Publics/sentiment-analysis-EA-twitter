9dbd8525fe4f6b0e3eedd68646c53d83a69db9a8
Nathanael Sheehan <nathanaelsheehan@gmail.com>
2021-06-24 22:59:15 +0100

    change source ref

--- code/build.R
+++ code/build.R
@@ -3,4 +3,4 @@ source("code/install_libs.R")
 # Get Twitter Token -------------------------------------------------------
 source("code/twitter_mine.R")
 # Database connection -----------------------------------------------------
-source("code/db.r")
+source("code/sentiment.r")
