75bae3edc3495633e57588741e711b4e1d977af2
Nathanael Sheehan <nathanaelsheehan@gmail.com>
2021-06-24 23:13:38 +0100

    programatically install/load pkgs

--- code/install_libs.R
+++ code/install_libs.R
@@ -1,7 +1,12 @@
-pkgs = c("tidyverse","academictwitteR")
-library(tidyverse)
-library(academictwitteR)
-library(tm)
-library(tidytext)
-library(sentimentr)
-library(treemap)
+pkgs = c("tidyverse", "academictwitteR", "tm", 
+         "tidytext", "sentimentr", "treemap"
+         )
+
+# Install packages not yet installed
+installed_packages = pkgs %in% rownames(installed.packages())
+if (any(installed_packages == FALSE)) {
+  install.packages(pkgs[!installed_packages])
+}
+
+# Packages loading
+invisible(lapply(pkgs, library, character.only = TRUE))
