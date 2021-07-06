b3e61b8ee0c9c715b68023abee43f582c7f04bf3
Nathanael Sheehan <nathanaelsheehan@gmail.com>
2021-07-06 19:06:02 +0100

    merge linguistics and sentiment into the same df

--- test/entity.r
+++ test/entity.r
@@ -1,5 +1,27 @@
-spacy_install()
+# for first use run: spacy_install()
+# & spacy_initialize()
 
-sample_data = sample_n(tweets_text_single,5)
+#sample of data
+sample_data = sample_n(tweets_text_single,
+                       1)
 
-parsedtxt = spacy_parse(sample_data$text)
+#Parse text and calculate computational linguistics using spacy.io API
+parsedtxt = spacy_parse(
+  sample_data$text,
+  pos = TRUE,
+  tag = TRUE,
+  lemma = TRUE,
+  entity = TRUE,
+  dependency = TRUE,
+  nounphrase = TRUE,
+  multithread = TRUE
+) %>% rename(word = token) %>% left_join(get_sentiments("nrc"))
+# Get sentiment with largest count
+emotion = names(table(parsedtxt$sentiment))[as.vector(table(parsedtxt$sentiment))==max(table(parsedtxt$sentiment))]
+ 
+# Logical rules for entity
+# 1. Calculate key sentiment (sentiment with the largest number of counts)
+# 2. Get the nounphrase
+# 3. Add the key sentiment, @ and the  nounphrase to create string e.g. "angry @ supply network" 
+# 4. Add polarity sentiment score
+# 
