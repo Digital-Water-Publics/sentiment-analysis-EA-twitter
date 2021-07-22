792ee75f2602e7467df1150f2165436ce446615c
Nathanael Sheehan <nathanaelsheehan@gmail.com>
2021-07-22 14:34:17 +0100

    nounphrase ngram plots

--- code/n_grams.r
+++ code/n_grams.r
@@ -1,14 +1,12 @@
-# Split corpus into 2 ngrams ----------------------------------------------
+# Calculate ngrams for single words----------------------------------------------
 if (file.exists("data/bigrams_filtered.RDS")) {
   bigrams_filtered = readRDS("data/bigrams_filtered.RDS")
 } else {
   ntweet_bigrams = tweets_primary_df %>%
-    unnest_tokens(biagram, word, token = "ngrams", n = 3)
-  
-  ntweet_bigrams_test =  ntweet_bigrams %>% filter(str_detect(biagram, "water industry"))
+    unnest_tokens(biagram, word, token = "ngrams", n = 2)
   
   bigrams_separated = ntweet_bigrams %>%
-    separate(biagram, c("word1", "word2", "word3"), sep = " ")
+    separate(biagram, c("word1", "word2"), sep = " ")
   
   bigrams_filtered = bigrams_separated %>%
     filter(!word1 %in% stop_words$word) %>%
@@ -16,20 +14,48 @@ if (file.exists("data/bigrams_filtered.RDS")) {
   
   saveRDS(bigrams_filtered, "data/bigrams_filtered.RDS")
 }
-# Create function to show ngram frequency ---------------------------------
 
-bigram_plots = function(word) {
+# Calculate ngrams for nounphrases ----------------------------------------
+nounphrase = as.data.frame(c("water industry", "water companies", "raw sewage","water quality" ))
+colnames(nounphrase) = "nounphrase"
+for(i in 1:nrow(nounphrase)){
+  nounphrase$phrasetoken[i] = str_replace_all(nounphrase$nounphrase[i], fixed(" "), "")
+}
+
+if(file.exists("data/bigrams_filtered_nounphrases.RDS")){
+  bigrams_filtered_3 = readRDS("data/bigrams_filtered_nounphrases.RDS")
+} else {
+  #create temp df and replace all nounphrases with thier squished tokens
+  n_gram_df = tweets_primary_df
+  for(i in 1:nrow(nounphrase)){
+    n_gram_df$word = str_replace_all(n_gram_df$word, nounphrase$nounphrase[i], nounphrase$phrasetoken[i])
+  }
+  ntweet_bigrams_nounphrase = n_gram_df %>%
+    unnest_tokens(biagram, word, token = "ngrams", n = 2)
+  
+  bigrams_separated_nounphrase = ntweet_bigrams_nounphrase %>%
+    separate(biagram, c("word1", "word2"), sep = " ")
+  
+  bigrams_separated_nounphrase = bigrams_separated_nounphrase %>%
+    filter(!word1 %in% stop_words$word) %>%
+    filter(!word2 %in% stop_words$word)
+  
+  saveRDS(bigrams_separated_nounphrase, "data/bigrams_filtered_nounphrases.RDS")
+}
+
+# Create function to show ngram frequency ---------------------------------
+bigram_plots = function(data, word) {
   plots_folder = "plots/"
   first_word = "1st_word"
   second_word = "2nd_word"
   
   #word n_gram as first place
-  word_graph_word_1 = bigrams_filtered %>%
+  word_graph_word_1 = data %>%
     filter(word1 == word) %>%
     count(word2, sort = TRUE)
   
   #Plot
-  word_graph_word_1 = word_graph_word_1[(1:25), ]
+  word_graph_word_1 = word_graph_word_1[(1:25),]
   
   ggplot(word_graph_word_1, aes(x = reorder(word2, n), y = n)) +
     geom_bar(stat = "identity") +
@@ -39,12 +65,12 @@ bigram_plots = function(word) {
   ggsave(paste(plots_folder, word, "first_word.png", sep = ""))
   
   #word n_gram as second place
-  word_graph_word_2 = bigrams_filtered %>%
+  word_graph_word_2 = data %>%
     filter(word2 == word) %>%
     count(word1, sort = TRUE)
   
   #Plot
-  word_graph_word_2 = word_graph_word_2[(1:25), ]
+  word_graph_word_2 = word_graph_word_2[(1:25),]
   ggplot(word_graph_word_2, aes(x = reorder(word1, n), y = n)) +
     geom_bar(stat = "identity") +
     xlab(paste(word, second_word, sep = " ")) +
@@ -53,11 +79,17 @@ bigram_plots = function(word) {
   ggsave(paste(plots_folder, word, "second_word.png", sep = ""))
 }
 
-bigram_plots(word = "river")
-bigram_plots(word = "flood")
-bigram_plots(word = "water")
+# Calculate colocation for sinlge phrases ------------------------------------
+for(i in 1:nrow(nounphrase)){
+  bigram_plots(data = bigrams_separated_nounphrase, word = nounphrase$phrasetoken[i])
+}
+# Calculate colocation for nounphrases ------------------------------------
+words = as.data.frame(c("river", "flood", "water", "waste", "pollution", "sewage", "bad"))
+colnames(words) = "word"
+for(i in 1:nrow(words)){
+  bigram_plots(data = bigrams_filtered, word = words$word[i])
+}
 
-bigram_plots(word = "waste")
-bigram_plots(word = "pollution")
-bigram_plots(word = "sewage")
+
+
 
