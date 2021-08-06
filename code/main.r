462d341cfe4d2f59bc6b1f1e8e2eb835ef0a625c
Nathanael Sheehan <nathanaelsheehan@gmail.com>
2021-08-06 16:57:23 +0100

    move clean tweets function

--- code/main.r
+++ code/main.r
@@ -1,3 +1,24 @@
+clean_tweets_sentiment = function(x) {
+  x %>%
+    # Remove URLs
+    str_remove_all(" ?(f|ht)(tp)(s?)(://)(.*)[.|/](.*)") %>%
+    # Remove mentions e.g. "@my_account"
+    str_remove_all("@[[:alnum:]_]{4,}") %>%
+    # Remove hashtags
+    str_remove_all("#[[:alnum:]_]+") %>%
+    # Replace "&" character reference with "and"
+    str_replace_all("&amp;", "and") %>%
+    # Remove puntucation, using a standard character class
+    str_remove_all("[[:punct:]]") %>%
+    # Remove "RT: " from beginning of retweets
+    str_remove_all("^RT:? ") %>%
+    # Replace any newline characters with a =space
+    str_replace_all("\\\n", " ") %>%
+    # Make everything lowercase
+    str_to_lower() %>%
+    # Remove any trailing whitespace around the text
+    str_trim("both")
+}
 #Read dataset and EA accounts
 ea_accounts = readRDS("data/EA_accounts.rds")
 all_tweets = read.csv("data/ea_mentions_all.csv")
--- code/setup.r
+++ code/setup.r
@@ -1,27 +1,4 @@
 #Clean tweets for sentiment
-clean_tweets_sentiment = function(x) {
-  x %>%
-    # Remove URLs
-    str_remove_all(" ?(f|ht)(tp)(s?)(://)(.*)[.|/](.*)") %>%
-    # Remove mentions e.g. "@my_account"
-    str_remove_all("@[[:alnum:]_]{4,}") %>%
-    # Remove hashtags
-    str_remove_all("#[[:alnum:]_]+") %>%
-    # Replace "&" character reference with "and"
-    str_replace_all("&amp;", "and") %>%
-    # Remove puntucation, using a standard character class
-    str_remove_all("[[:punct:]]") %>%
-    # Remove "RT: " from beginning of retweets
-    str_remove_all("^RT:? ") %>%
-    # Replace any newline characters with a =space
-    str_replace_all("\\\n", " ") %>%
-    # Make everything lowercase
-    str_to_lower() %>%
-    # Remove any trailing whitespace around the text
-    str_trim("both")
-}
-
-
 remove_known_ea_accounts = function(){
   ea_accounts = readRDS("data/EA_accounts.rds")
   all_tweets = read.csv("data/ea_mentions_all.csv")
