7968d44d02b7a605a8e7fe359d715b532d3b17e8
Nathanael Sheehan <nathanaelsheehan@gmail.com>
2021-08-16 15:38:20 +0100

    flood sentiment progress

--- code/floods.r
+++ code/floods.r
@@ -12,37 +12,28 @@ floods_study_year = floods %>% filter(year > 2015) %>%
   filter(flood_caus != "unknown") %>%
   filter(fluvial_f == "True") 
 
-floods_ok = floods_study_year %>% select(name,start_date,end_date) %>% st_drop_geometry() %>% distinct(name, .keep_all = TRUE)
-
-
-floods_study_year$name = unique(floods_study_year$name)
-
+floods_ok = floods_study_year %>% select(name,start_date,end_date) %>% 
+  st_drop_geometry() %>% 
+  distinct(name, .keep_all = TRUE)
 
 # create day events of sentiment scores
 sentiment_history = df_no_ea %>%
   group_by(date) %>%
   summarise(across(anticipation:surprise, ~ sum(.x, na.rm = FALSE)))
 
-
-floods_study_year$start_date = as.Date(floods_study_year$start_date)
-sentiment_history$date = as.Date(sentiment_history$date)
-df_no_ea$start_date = format(df_no_ea$date, "%Y-%m-%d")
-
-floods_study_year %>% left_join(sentiment_history) %>%
-  filter(between(date, start_date, end_date)) %>%
-           group_by(date) %>%
-           mutate(sum_amount = sum(amount))
-
-
-
-tt %>% filter(between(start_date,end_date)) %>%
-  group_by(start_date) %>%
-  mutate(sum_amount = sum(amount))
-
-tt = tt %>% distinct(name) %>% pull()
-
-## calculate sentiment count for the start to end date of each flood incident
-## use n to calculate frequency of tweets during event
-## plot map of rivers and their popularity 
-
-
+popo = floods_ok %>% crossing(sentiment_history)  %>%
+  mutate(across(c(start_date, end_date, date), ymd),
+         interval = interval(start_date, end_date)) %>%
+  filter(date %within% interval) %>%
+  group_by(interval) %>%
+  mutate(sum_anger = sum(anger)) %>%
+  mutate(sum_anticipation = sum(anticipation)) %>%
+  mutate(sum_joy = sum(joy)) %>%
+  mutate(sum_trust = sum(trust)) %>%
+  mutate(sum_fear = sum(fear)) %>%
+  mutate(sum_sadness = sum(sadness)) %>%
+  mutate(sum_disgust = sum(disgust)) %>%
+  mutate(sum_surprise = sum(surprise)) %>%
+  select(name,sum_anger,sum_anticipation,sum_joy,sum_trust,sum_fear,sum_sadness,sum_disgust,sum_surprise) %>%
+  distinct(name, .keep_all = TRUE) %>%
+  arrange(interval)
