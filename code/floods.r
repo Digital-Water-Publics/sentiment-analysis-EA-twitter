7c2674954f2f753b6ea4700a6fa1691c947ec2a5
Nathanael Sheehan <nathanaelsheehan@gmail.com>
2021-08-16 15:38:54 +0100

    styling for flood script

--- code/floods.r
+++ code/floods.r
@@ -1,19 +1,21 @@
 library(sf)
 # read data
-floods = read_sf("../../Documents/EA_RecordedFloodOutlines_SHP_Full/data/Recorded_Flood_Outlines.shp")
+floods = read_sf(
+  "../../Documents/EA_RecordedFloodOutlines_SHP_Full/data/Recorded_Flood_Outlines.shp"
+)
 # create year column
-floods$year = substr(floods$start_date,1,4)
+floods$year = substr(floods$start_date, 1, 4)
 floods$year = as.numeric(floods$year)
 
-# filter based on study timeline 
+# filter based on study timeline
 floods_study_year = floods %>% filter(year > 2015) %>%
-  filter(year < 2022) %>% 
+  filter(year < 2022) %>%
   filter(hfm_status == "considered and accepted") %>%
   filter(flood_caus != "unknown") %>%
-  filter(fluvial_f == "True") 
+  filter(fluvial_f == "True")
 
-floods_ok = floods_study_year %>% select(name,start_date,end_date) %>% 
-  st_drop_geometry() %>% 
+floods_ok = floods_study_year %>% select(name, start_date, end_date) %>%
+  st_drop_geometry() %>%
   distinct(name, .keep_all = TRUE)
 
 # create day events of sentiment scores
@@ -34,6 +36,16 @@ popo = floods_ok %>% crossing(sentiment_history)  %>%
   mutate(sum_sadness = sum(sadness)) %>%
   mutate(sum_disgust = sum(disgust)) %>%
   mutate(sum_surprise = sum(surprise)) %>%
-  select(name,sum_anger,sum_anticipation,sum_joy,sum_trust,sum_fear,sum_sadness,sum_disgust,sum_surprise) %>%
+  select(
+    name,
+    sum_anger,
+    sum_anticipation,
+    sum_joy,
+    sum_trust,
+    sum_fear,
+    sum_sadness,
+    sum_disgust,
+    sum_surprise
+  ) %>%
   distinct(name, .keep_all = TRUE) %>%
   arrange(interval)
