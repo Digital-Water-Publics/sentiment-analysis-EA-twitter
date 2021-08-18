89000199f35d82945214fe02ba847e69602d32b5
Nathanael Sheehan <nathanaelsheehan@gmail.com>
2021-08-18 01:42:18 +0100

    clean up

--- code/build.R
+++ code/build.R
@@ -14,31 +14,25 @@ source("code/build_setup.r")
 source("code/n_grams.r")
 
 # Calculate collocation for bigram phrases
-suppressWarnings(
-  suppressMessages(for (i in 1:nrow(nounphrase)) {
-    bigram_plots(data = bigrams_filtered, word = nounphrase$phrasetoken[i])
-  }))
+suppressWarnings(suppressMessages(for (i in 1:nrow(nounphrase)) {
+  bigram_plots(data = bigrams_filtered, word = nounphrase$phrasetoken[i])
+}))
 
 # Calculate colocation for nounphrases
 words = as.data.frame(c("river", "flood", "water", "waste", "pollution", "sewage", "bad"))
 colnames(words) = "word"
-suppressWarnings(
-  suppressMessages(for (i in 1:nrow(words)) {
-    bigram_plots(data = bigrams_filtered_3, word = words$word[i])
-  })
-)
+suppressWarnings(suppressMessages(for (i in 1:nrow(words)) {
+  bigram_plots(data = bigrams_filtered_3, word = words$word[i])
+}))
 # remove dataframes
 rm(bigrams_filtered, bigrams_filtered_3, words, nounphrase)
 
-
 # Semantic Frequency ------------------------------------------------------
-
 source("code/semantic_freq.r")
 
-# OPTIONAL 
+# OPTIONAL
 # Topic modelling -----------------------------------------------------------------
 source("code/topic_modelling.r")
 #topic_model_tweet_corpus(tweet_vector = tweets_primary_df$word)
 # Networks ----------------------------------------------------------------
 source("code/networks.R")
-
